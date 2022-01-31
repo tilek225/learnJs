39a4b90d78503e2a8c8005476465cb0e6717259d
Tilek <askaralievtilek@gmail.com>
2022-01-31 18:01:19 +0300

    first lesson

---
+++ index.js
@@ -0,0 +1,40 @@
+// let type1 = undefined; // не определено
+// let type2 = 'Maks'; // строка
+// let type3 = 100; // число
+// let type4 = null; // пустое значение - ноль
+// let type5 = true; // boolean - логическое значение true/false
+// let type6 = {}; // объект - набор данных
+
+// //typeof
+// console.log(typeof(type1)); // возращает тип данных
+// console.log(typeof type2);
+
+// let a = '10';
+// let b = 10;
+// console.log(a == b);
+
+// let num1 = 10;
+// let num2 = 5;
+// console.log(num1 > num2);
+
+// let num1 = '10'; 
+// let num2 = 10;
+// console.log(num1 !== num2);
+
+// мат. операторы
+// let num1 = 10;
+// let num2 = 5;
+// console.log(num1**num2);
+
+// let num1 = 5; 
+// let num2 = 'а';
+// console.log(num1 - num2); // NaN - not a number
+// console.log(num1 + num2); //конкатенация
+
+// console.log(isNaN(20));
+
+// let num1 = 5;
+// // num1 = num1 + 10;
+// num1+=10;
+// console.log(num1);
+
