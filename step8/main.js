96b6ff9992827ff0fcb1b672210fc36e95b67321
Тилек <71582021+tilek225@users.noreply.github.com>
2022-02-21 17:26:58 +0300

    step 8

---
+++ step8/main.js
@@ -0,0 +1,70 @@
+// Колбэк функции
+
+// Map
+// let fruits = ['banana', 'pineapple', ' cocos'];
+// fruits = fruits.map((item, idx, array) => {
+//     return item + 1
+// });
+// console.log(fruits);
+
+// let arr = [1, 2, true, false];
+// arr = arr.map((item) => {
+//     if(typeof item === 'number'){
+//         return 'Hello'
+//     }
+//     return item;
+// });
+
+// const task1 = (arr) => {
+//     return arr.map((item, idx) => {
+//         if(typeof item === 'boolean'){
+//             return idx
+//         }
+//         return item
+//     })
+// };
+// console.log(task1([1,2,, false, true]));
+
+// filter
+// const filtetMethod = (arr) => {
+//     arr.filter((item, idx, array) => {
+//         return typeof item === 'string'
+//     })
+// }
+// console.log('Ivan', 'Kolya', 'Sasha',5, 6, 2);
+
+// find
+// const findMethod = (arr) => {
+//     return arr.find((item, idx) => {
+//         return item > 10
+//     })
+// };
+// console.log(findMethod([1, 2, 3, 4, 5, 6, 7]));
+
+// const findMethod = (arr) => {
+//     return arr.findIndex((item, idx) => {
+//         return item > 10
+//     })
+// };
+// console.log(findMethod([1, 2, 3, 4, 5, 6, 7]));
+//
+
+// sort
+// const sortMethod = (arr) => {
+//     arr.sort((a, b) => {
+//         if( a > b){
+//             return 1
+//         } else if (a < b){
+//             return -1
+//         }
+//         return 0
+//     })
+// };
+// console.log(sortMethod(['Ivan', 'Kolya', 'Alex']));
+
+
+// const task1 = (arr) => {
+//     return arr.filter(item =>  item.length > 4 )
+// };
+// console.log(task1(['Honda', 'Mers', 'BMW', 'Porshe']));
+
