0b8260d7484e3ab735768be3354c8db624271755
Тилек <71582021+tilek225@users.noreply.github.com>
2022-03-11 20:54:23 +0600

    step 11

--- step11/main.js
+++ step11/main.js
@@ -78,3 +78,119 @@
 // // Проверить на наличие
 // console.log(p.getAttribute('id'));
 // Получить атрибут
+
+// let h3 = document.createElement('h3'); // Создание нового тега
+// let title = document.querySelector('.title');
+// let body = document.body;
+//
+// // title.after(h3); // Добавление элемента после
+// // title.before(h3); // Добавление элемента до
+//
+// h3.textContent = 'Новый элемент';
+
+
+// body.appendChild(h3);// Добавление элемента в конец
+// body.append(h3);// Добавление элемента в конец ++ он принимает ввиде элементво обычные строки
+// body.prepend(h3); // Добавление элемента в начале
+
+// let box = document.querySelector('#box');
+// let hello = document.querySelector('.hi');
+
+// box.removeChild(hello); // Удалить ребенка обращаясь к родителю
+// hello.remove(); // Удалить элемент
+
+// console.log(box.children); // получение потомков
+// console.log(box.childNodes); // получение потомков
+
+// console.log(box.childElementCount); // получение количества потомков
+
+// console.log(hello.parentElement); // получение родителя
+// console.log(hello.parentNode);  // получение родителя
+
+// console.log(hello.nextElementSibling);// получение соседа после тебя
+// console.log(hello.previousElementSibling); // получение соседа до тебя
+
+
+// console.log(input.value); // получение значения у поля ввода
+// console.log(input.checked); // проверка на установку
+
+
+// btn.onclick = () => {
+//     alert('ihi')
+// };
+
+// btn.onmouseover = () => {
+//    h1.style.background = 'red'
+// };
+//
+// btn.onmouseleave = () => {
+//     h1.style.background = 'transparent'
+// };
+//
+// btn.ondblclick = () => {
+//     alert('hlkj;lk');
+// };
+//
+//
+// let input = document.querySelector('input');
+// let btn = document.querySelector('button');
+// let agent = document.querySelector('#agent007');
+// let agentSpan = document.querySelector('#agent007 span');
+//
+// btn.addEventListener('click', (event) => {
+//     if(input.value.length > 0) {
+//         agentSpan.textContent = input.value;
+//         input.value = '';
+//     }
+// });
+//
+// input.addEventListener('change', (e) => {
+//     agentSpan.textContent = input.value;
+//
+//     if(input.value.length < 1){
+//         btn.disabled = true;
+//     }else{
+//         btn.disabled = false;
+//     }
+// });
+//
+// let form = document.querySelector('form');
+// let inputText = document.querySelector('#input1');
+// let checkBox = document.querySelector('#check1');
+// let btnSub = document.querySelector('#sub');
+//
+// form.addEventListener('submit', (e) => {
+//     e.preventDefault(); // функция для для запрета перезагрузки сайта
+//     // console.log(e.target[0].value); // получение первого значения в форме
+//     // console.log(e.target[1].checked); // получение второго значения в форме
+//
+//     if(e.target[1].checked) {
+//         agentSpan.textContent = e.target[0].value;
+//         e.target[0].value = '';
+//         e.target[1].checked = false;
+//     } else{
+//         alert('поставьте галочку')
+//     }
+// });
+
+let form = document.querySelector('form');
+let listItem = document.querySelectorAll('li')
+let list = document.querySelector('ul');
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    let li = document.createElement('li');
+    li.textContent = e.target[0].value;
+    list.append(li);
+    e.target[0].value = ''
+});
+
+Array.from(listItem).forEach((item) => {
+    item.style.cursor = 'pointer';
+    item.addEventListener('click', (e) => {
+      item.style.color = `
+        rgb(${Math.floor(Math.random() * 225)}, ${Math.floor(Math.random() * 225)}, ${Math.floor(Math.random() * 225)})
+        `
+    })
+});
