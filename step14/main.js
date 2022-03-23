b979a8d7dc0b2932f4dce04275e10d411e800255
Tilek <askaralievtilek@gmail.com>
2022-03-23 21:20:15 +0600

    fetch

--- step14/main.js
+++ step14/main.js
@@ -23,12 +23,26 @@
 
 
 let list = document.querySelector('ul');
-let btn = document.querySelector('button');
+let btn = document.querySelector('.btn');
+let form = document.querySelector('form');
+let h2 = document.querySelector('h2')
+
 
 btn.addEventListener('click', e => {
     fetch('https://jsonplaceholder.typicode.com/posts')
         .then(res => res.json())
         .then(data => data.forEach((item, idx) => {
-            list.innerHTML
-        }))
+            list.innerHTML += `
+                <li>${idx + 1}) ${item.title}</li>
+            `
+        })).catch(err => alert('упс произошла ошибка'))
 })
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+    fetch(`https://jsonplaceholder.typicode.com/posts/${e.target[0].value}`)
+        .then(res => res.json())
+        .then(data => {
+            h2.textContent = data.title
+        }).catch(err => alert('undefined'))
+});
